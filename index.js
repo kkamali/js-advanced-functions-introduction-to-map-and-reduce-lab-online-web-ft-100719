586228cc957bc77e4ee20dc91928d3b4c1ad2a47
Kathleen Kamali <kathleenkamali@gmail.com>
2020-02-06 10:44:28 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+  const newArray = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArray.push(sourceArray[i] * -1)
+  }
+  return newArray
+}
+
+function mapToNoChange(sourceArray) {
+  const newArray = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArray.push(sourceArray[i])
+  }
+  return newArray
+}
+
+function mapToDouble(sourceArray) {
+  const newArray = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArray.push(sourceArray[i] * 2)
+  }
+  return newArray
+}
+
+function mapToSquare(sourceArray) {
+  const newArray = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArray.push(sourceArray[i] * sourceArray[i])
+  }
+  return newArray
+}
+
+function reduceToTotal(sourceArray, total = 0) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    total = total + sourceArray[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (!sourceArray[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i]) {
+      return true
+    }
+  }
+  return false
+}
